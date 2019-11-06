90ef653eb67aedd25f75e88f666917b6583ddd45
wjh123 <894275157@qq.com>
2019-11-06 18:19:38 +0800

    配置docker webhook

---
+++ dockerUpdate.js
@@ -0,0 +1,23 @@
+// const process = require('child_process')
+const Koa = require('koa')
+const Router = require('koa-router')
+
+const app = new Koa()
+const router = new Router()
+app.use(router.routes())
+
+// const instruct = [
+//   'docker-compose pull',
+//   'docker-compose stop',
+//   'docker-compose rm',
+//   'docker-compose up -d'
+// ]
+
+router.post('/wechat', ctx => {
+  console.log(ctx)
+})
+
+// process.exec('docker-compose pull info', () => {
+// })
+
+app.listen('2333')
